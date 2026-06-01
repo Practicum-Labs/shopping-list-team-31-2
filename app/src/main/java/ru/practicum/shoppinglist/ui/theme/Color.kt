b8a3b7bfe62504629679764b619b402eba6a19a4
novinnik <novinnik@yandex.ru>
2026-06-01 21:59:38 +0300

    Созданы компоненты светлой/темной темы Добавлены иконки Добавлены цвета Добавленыы шрифты Добавлены строковые параметры

--- app/src/main/java/ru/practicum/shoppinglist/ui/theme/Color.kt
+++ app/src/main/java/ru/practicum/shoppinglist/ui/theme/Color.kt
@@ -2,10 +2,28 @@ package ru.practicum.shoppinglist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Background Theme
+val ColorLightPrimary = Color(0xFFFFF8F4)
+val ColorDarkPrimary = Color(0xFF19120C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Container
+val ColorBackgroundContainerLight = Color(0xFFF4E6DA)
+val ColorBackgroundContainerDark = Color(0x21FF3029)
+
+val LightContainer = Color(0xFFFFDCBB)
+val DarkContainer = Color(0xFFBE8543)
+
+val RegularElementLight = Color(0xFF845416)
+val RegularElementDark = Color(0xFFE0C1A3)
+
+// Text
+val DarkText = Color(0xFF211A14)
+val LightText = Color(0xFFEEE0D5)
+
+val SmallDarkText = Color(0xFFE5C5A7)
+val MediumLightText = Color(0xFF50453A)
+
+val SchemesOnSurface = Color(0xFF1D1B20)
+val NoActiveElement = Color(0xFFFEDDBD)
+val SurfaceContainerHigh = Color(0xFFF4E6DA)
+
