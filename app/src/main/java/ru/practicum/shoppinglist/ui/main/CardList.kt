a8b6da68cebe583cf88747fd7fa20ed44e6735d1
novinnik <novinnik@yandex.ru>
2026-06-17 02:00:49 +0300

    Свайп влево и вправо свыше половины экрана. Добавлены необходимые цвета и константы

--- app/src/main/java/ru/practicum/shoppinglist/ui/main/CardList.kt
+++ app/src/main/java/ru/practicum/shoppinglist/ui/main/CardList.kt
@@ -24,26 +24,37 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import ru.practicum.shoppinglist.R
+import ru.practicum.shoppinglist.ui.main.Variables.ACTION_BUTTON_WIDTH
 import ru.practicum.shoppinglist.ui.main.Variables.ANIMATION_SPEC
 import ru.practicum.shoppinglist.ui.main.Variables.MIN_OFFSET
+import ru.practicum.shoppinglist.ui.main.Variables.SWIPE_ACTION_THRESHOLD
+import ru.practicum.shoppinglist.ui.main.Variables.VISIBLE_PART_WIDTH
 import ru.practicum.shoppinglist.ui.theme.ShoppingListTheme
 import kotlin.math.roundToInt
+
+const val SWIPE_NONE = "none"
+const val SWIPE_SHORT_LEFT = "short_left"
+const val SWIPE_FULL_LEFT = "full_left"
+const val SWIPE_FULL_RIGHT = "full_right"
 
 @Composable
 fun CardList(
@@ -59,12 +70,24 @@ fun CardList(
     val scope = rememberCoroutineScope()
     val offsetX = remember { Animatable(0f) }
     val density = LocalDensity.current
-    val actionButtonsWidth = 168.dp
+    var fullScreenWidthPx by remember { mutableStateOf(0) }
+
+    var swipeState by remember { mutableStateOf("none") }
 
     val closeActions: () -> Unit = {
         scope.launch {
             offsetX.animateTo(0f, animationSpec = tween(ANIMATION_SPEC))
         }
+    }
+
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .onSizeChanged { size ->
+                    fullScreenWidthPx = size.width
+                }
+        )
     }
 
     ShoppingListTheme {
@@ -77,7 +100,8 @@ fun CardList(
                 onEdit = onEdit,
                 onCopy = onCopy,
                 onDelete = onDelete,
-                onClose = closeActions
+                onClose = closeActions,
+                swipeState = swipeState
             )
 
             Card(
@@ -87,11 +111,15 @@ fun CardList(
                     .offset { IntOffset(offsetX.value.roundToInt(), 0) }
                     .clickable { onCardClick() }
                     .swipeToRevealActions(
-                        offsetX = offsetX,
-                        density = density,
-                        actionWidth = actionButtonsWidth,
-                        onSwipeStateChange = { },
-                        coroutineScope = scope
+                        config = SwipeConfig(
+                            offsetX = offsetX,
+                            density = density,
+                            onSwipeState = { newState -> swipeState = newState },
+                            scope = scope,
+                            onSwipeLeft = { },
+                            onSwipeRight = onEdit,
+                            fullWidthPx = fullScreenWidthPx
+                        )
                     ),
                 shape = RoundedCornerShape(12.dp),
                 elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
@@ -108,52 +136,81 @@ fun CardList(
 }
 
 fun Modifier.swipeToRevealActions(
-    offsetX: Animatable<Float, *>,
-    density: Density,
-    actionWidth: Dp,
-    onSwipeStateChange: (Boolean) -> Unit,
-    coroutineScope: CoroutineScope
+    config: SwipeConfig
 ): Modifier = this.pointerInput(Unit) {
+    val fullSwipeThresholdPx =
+        if (config.fullWidthPx > 0) config.fullWidthPx / 2f else SWIPE_ACTION_THRESHOLD
+    val maxOffset = with(density) { ACTION_BUTTON_WIDTH.dp.toPx() }
+    val visiblePartWidthPx = with(density) { VISIBLE_PART_WIDTH.dp.toPx() }
+
+    fun animateToClosedState() {
+        val finalStatus = getFinalState(config, fullSwipeThresholdPx)
+        config.onSwipeState(finalStatus)
+    }
+
+    suspend fun handleDragEnd(currentValue: Float) {
+        var finalStatus = SWIPE_NONE
+        if (currentValue <= -fullSwipeThresholdPx) {
+            config.onSwipeLeft()
+            val targetPosition = -config.fullWidthPx + visiblePartWidthPx
+            config.offsetX.animateTo(targetPosition, animationSpec = tween(ANIMATION_SPEC))
+            finalStatus = SWIPE_FULL_LEFT
+        } else if (currentValue >= fullSwipeThresholdPx) {
+            config.onSwipeRight()
+            val targetPosition = config.fullWidthPx - visiblePartWidthPx
+            config.offsetX.animateTo(targetPosition, animationSpec = tween(ANIMATION_SPEC))
+            finalStatus = SWIPE_FULL_RIGHT
+        } else {
+            if (currentValue < MIN_OFFSET) {
+                config.offsetX.animateTo(-maxOffset, animationSpec = tween(ANIMATION_SPEC))
+                finalStatus = SWIPE_SHORT_LEFT
+            } else {
+                config.offsetX.animateTo(0f, animationSpec = tween(ANIMATION_SPEC))
+            }
+        }
+        config.onSwipeState(finalStatus)
+    }
+
     detectHorizontalDragGestures(
         onDragStart = {
-            coroutineScope.launch { offsetX.stop() }
+            config.scope.launch { config.offsetX.stop() }
         },
         onDragEnd = {
-            coroutineScope.launch {
-                val maxOffset = -with(density) { actionWidth.toPx() }
-                val isOpen = offsetX.value < MIN_OFFSET
-
-                if (isOpen) {
-                    offsetX.animateTo(maxOffset, animationSpec = tween(ANIMATION_SPEC))
-                    onSwipeStateChange(true)
-                } else {
-                    offsetX.animateTo(0f, animationSpec = tween(ANIMATION_SPEC))
-                    onSwipeStateChange(false)
-                }
+            config.scope.launch {
+                handleDragEnd(config.offsetX.value)
             }
         },
         onDragCancel = {
-            coroutineScope.launch {
-                offsetX.animateTo(0f, animationSpec = tween(ANIMATION_SPEC))
-                onSwipeStateChange(false)
+            config.scope.launch {
+                config.offsetX.animateTo(0f, animationSpec = tween(ANIMATION_SPEC))
+                animateToClosedState()
             }
         },
         onHorizontalDrag = { _, dragAmount ->
-            coroutineScope.launch {
-                val maxOffset = -with(density) { actionWidth.toPx() }
-                val newOffset = offsetX.value + dragAmount
-                offsetX.snapTo(newOffset.coerceIn(maxOffset, 0f))
+            config.scope.launch {
+                val newOffset = config.offsetX.value + dragAmount
+                val clampedValue = newOffset.coerceIn(-fullSwipeThresholdPx, fullSwipeThresholdPx)
+                config.offsetX.snapTo(clampedValue)
+                animateToClosedState()
             }
         }
     )
 }
+
 @Composable
 private fun SwipeActions(
     onEdit: () -> Unit,
     onCopy: () -> Unit,
     onDelete: () -> Unit,
-    onClose: () -> Unit
+    onClose: () -> Unit,
+    swipeState: String
 ) {
+    var arrangementCenter = Arrangement.End
+
+    if (swipeState == SWIPE_FULL_LEFT) {
+        arrangementCenter = Arrangement.Center
+    }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -162,15 +219,44 @@ private fun SwipeActions(
                 color = MaterialTheme.colorScheme.primary,
                 shape = RoundedCornerShape(12.dp)
             ),
-        horizontalArrangement = Arrangement.End,
+        horizontalArrangement = arrangementCenter,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        ActionButton(icon = R.drawable.ic_edit, description = "Edit", onClick = { onClose(); onEdit() })
-        Spacer(modifier = Modifier.width(8.dp))
-        ActionButton(icon = R.drawable.ic_copy, description = "Copy", onClick = { onClose(); onCopy() })
-        Spacer(modifier = Modifier.width(8.dp))
-        ActionButton(icon = R.drawable.ic_delete, description = "Delete", onClick = { onClose(); onDelete() })
-        Spacer(modifier = Modifier.width(16.dp))
+        when (swipeState) {
+            SWIPE_SHORT_LEFT -> {
+                ActionButton(
+                    icon = R.drawable.ic_edit,
+                    description = "Edit",
+                    onClick = { onClose(); onEdit() }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                ActionButton(
+                    icon = R.drawable.ic_copy,
+                    description = "Copy",
+                    onClick = { onClose(); onCopy() }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                ActionButton(
+                    icon = R.drawable.ic_delete,
+                    description = "Delete",
+                    onClick = { onClose(); onDelete() }
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+            }
+
+            SWIPE_FULL_LEFT -> {
+                ActionButton(
+                    icon = R.drawable.ic_delete,
+                    description = "Delete",
+                    onClick = { onDelete() },
+                    isSwipeFullLeft = true
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+            }
+
+            SWIPE_FULL_RIGHT, SWIPE_NONE -> {
+            }
+        }
     }
 }
 
@@ -219,12 +305,25 @@ private fun CardContent(
 }
 
 @Composable
-private fun ActionButton(icon: Int, description: String, onClick: () -> Unit) {
+private fun ActionButton(
+    icon: Int,
+    description: String,
+    onClick: () -> Unit,
+    isSwipeFullLeft: Boolean = false
+) {
+    var colorBackgroung = MaterialTheme.colorScheme.onSecondary
+    var colorTInt = MaterialTheme.colorScheme.secondary
+
+    if (isSwipeFullLeft) {
+        colorBackgroung = MaterialTheme.colorScheme.secondary
+        colorTInt = MaterialTheme.colorScheme.surfaceVariant
+    }
+
     Box(
         modifier = Modifier
             .size(40.dp)
             .background(
-                color = MaterialTheme.colorScheme.onSecondary,
+                color = colorBackgroung,
                 shape = CircleShape
             )
             .clickable(
@@ -237,10 +336,29 @@ private fun ActionButton(icon: Int, description: String, onClick: () -> Unit) {
             painter = painterResource(icon),
             contentDescription = description,
             modifier = Modifier.size(24.dp),
-            tint = MaterialTheme.colorScheme.secondary
+            tint = colorTInt
         )
     }
 }
+
+private fun getFinalState(config: SwipeConfig, fullSwipeThresholdPx: Float): String {
+    return when {
+        config.offsetX.value <= -fullSwipeThresholdPx -> SWIPE_FULL_LEFT
+        config.offsetX.value >= fullSwipeThresholdPx -> SWIPE_FULL_RIGHT
+        config.offsetX.value < 0 -> SWIPE_SHORT_LEFT
+        else -> SWIPE_NONE
+    }
+}
+
+data class SwipeConfig(
+    val offsetX: Animatable<Float, *>,
+    val density: Density,
+    val onSwipeState: (String) -> Unit,
+    val scope: CoroutineScope,
+    val onSwipeLeft: () -> Unit,
+    val onSwipeRight: () -> Unit,
+    val fullWidthPx: Int
+)
 
 @Preview(name = "CardList - Recipes", showBackground = true)
 @Composable
